b0b2082eb5768352477257d9adccf7cb7ff84e96
PhilippSpo <philipp.sporrer@planifica.de>
2015-02-10 14:18:45 +0100

    removed zoom limit, zoom to center when zooming out, removed size limit for canvas

--- package.js
+++ package.js
@@ -28,6 +28,7 @@ Package.onUse(function (api) {
   api.addFiles('lib/Shapes/Rectangle/Rect.js', 'client');
   api.addFiles('lib/Shapes/Rectangle/RectangleFunctions.js', 'client');
   api.addFiles('lib/Shapes/TextField/TextField.js', 'client');
+  api.addFiles('lib/Shapes/Icon/Icon.js', 'client');
   api.addFiles('lib/ReactiveCanvas.js', 'client');
   api.addFiles('lib/panZoomCanvas.js', 'client');
   api.addFiles('lib/CanvasController.js', 'client');
